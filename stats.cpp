f3195e4d087789bb7ce3330427734f31979021e5
pathi-manoj <91237206+pathi-manoj@users.noreply.github.com>
2021-10-24 13:24:43 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,21 +1,26 @@
 #include "stats.h"
 #include <bits/stdc++.h>
 #include <numeric>
+#include <vector>
 using namespace std;
   
 
 Stats Statistics::ComputeStatistics(const std::vector<float>& statsData ) {
     //Implement statistics here
     
+    struct Stats s;
     if ( statsData.empty() )
     {
-        statsData.min = NAN;
-        statsData.max = NAN;
-        statsData.average = NAN;
+        s.min = NAN;
+        s.max = NAN;
+        s.average = NAN;
+      
+      return s;
     }
     else {
-        statsData.min = *min_element( statsData.begin(), statsData.end());
-        statsData.max = *max_element( statsData.begin(), statsData.end());
-        statsData.average = reduce( statsData.begin(), statsData.end()) / statsData.size());  
+        s.min = *min_element( statsData.begin(), statsData.end());
+        s.max = *max_element( statsData.begin(), statsData.end());
+        s.average = reduce( statsData.begin(), statsData.end()) / statsData.size());  
+      return s;
     }
 }
