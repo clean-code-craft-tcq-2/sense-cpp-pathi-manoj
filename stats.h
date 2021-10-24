e2aabe1cfd0f3c93a083a25aeb3b4786df2b0d72
pathi-manoj <91237206+pathi-manoj@users.noreply.github.com>
2021-10-24 13:28:05 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -4,15 +4,15 @@ struct Stats {
     float average ;
     float max ;
     float min ;
-}
+};
 
 struct EmailAlert {
     bool emailSent ;
-}
+};
 
 struct LEDAlert {
     bool ledGlows ;
-}
+};
 namespace Statistics {
     Stats ComputeStatistics(const std::vector<float>& statsData );
 }
